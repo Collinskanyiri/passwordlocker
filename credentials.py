04ddb3cf9f9ee4981acc4cc8501af9a461bbec68
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-09-05 14:30:28 +0300

    update2.0

--- credentials.py
+++ credentials.py
@@ -1,37 +1,68 @@
+import unittest
+
 class Credentials:
-    """Class that generates new instances of credentials"""
+    """
+    Class that generates new instances of credentials
+    """
 
-    credentials_list = [] # Empty credentials list
+    credentials_list = []  # Empty credentials list
     # Init method up here
-    def save_credentials(self):
 
-        '''
+    
+    def __init__(self, credentials_user_name,fname, lname,email, credentials_site, credentials_password):
+
+
+        """
+        __init__ method to  specify the attributes of a User object
+
+        Args:
+            user_name = user name
+            email=email address
+            credentials_site = the name of the credentials acccount
+            credentials_password = the password of the account
+        """
+      
+        self.credentials_user_name = credentials_user_name
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.credentials_site = credentials_site
+        self.credentials_password = credentials_password
+        
+
+
+    def save_credentials(self):
+       '''
         save_credentials method save credentials objects into credentials_list
         '''
 
-        Credentials.credentials_list.append(self)
-
-    def __init__(self,first_name,last_name,user_name,password,email):
-
-      # docstring removed for simplicity 
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.user_name = user_name 
-        self.password = password
-        self.email = email
-
+       Credentials.credentials_list.append(self)
+    
     def delete_credentials(self):
-
         '''
         delete_credentials method deletes a saved credentials from the credentials_list
         '''
 
         Credentials.credentials_list.remove(self)
+
     @classmethod
-    def find_by_email(cls,email):
+    def display_credentials(cls, credentials_user_name):
+        """
+        method that will return the credentials list
+
+        """
+        user_credentials_list = []
+
+        for credentials in cls.credentials_list:
+            if credentials.user_name == credentials_user_name:
+                user_credentials_list.append(credentials)
+
+        return user_credentials_list
+
+    @classmethod
+    def find_by_email(cls, email):
         '''
-        Method that takes in a email and returns a credentials that matches that email.
+        Method that takes in a email and returns the credentials that matches that email.
 
         Args:
             email: email to search for
@@ -42,24 +73,50 @@ class Credentials:
         for credentials in cls.credentials_list:
             if credentials.email == email:
                 return credentials
+    
     @classmethod
-    def credentials_exist(cls,email):
+    def find_credentials(cls, credentials_site):
+        """
+        method that takes in credentials site and returns the credentials that matches that name.
+
+        Returns :
+            credentials name that matches the input given.
+        """
+
+        for credentials in cls.credentials_list:
+            if credentials_site == credentials_site:
+                return credentials
+
+    @classmethod
+    def credentials_exist(cls, email):
         '''
         Method that checks if a credentials exists from the credentials list.
-        Args:
-            email: email to search if it exists
+
         Returns :
             Boolean: True or false depending if the credentials exists
         '''
         for credentials in cls.credentials_list:
             if credentials.email == email:
-                    return True
+                return True
 
-        return False   
+        return False
+
     @classmethod
-    def display_credentials(cls):
-        '''
-        method that returns the credentials list
-        '''
-        return cls.credentials_list 
+    def credentials_exists(cls, credentials_site):
+        """
+        method to check for credentials
 
+        Returns:
+            Boolean: true/ false depending if the credentials exist
+        """
+
+        for credentials in cls.credentials_list:
+            if credentials.credentials_site == credentials_site:
+                return True
+
+        return False
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
